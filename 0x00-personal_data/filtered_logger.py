b4e83ea1cd31cf604ca0239d633c66fa9b7cb13f
kal-kyokya <kalkyokya4@gmail.com>
2024-11-12 11:30:01 +0000

    Implement a log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -28,7 +28,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields=None):
+    def __init__(self, fields: List[str]):
         """Constructor function for all class instances."""
         self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
