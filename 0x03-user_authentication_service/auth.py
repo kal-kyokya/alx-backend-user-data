c1400a646815c06dd20ffec567a4818c43116075
kal-kyokya <kalkyokya4@gmail.com>
2024-11-19 15:24:25 +0000

    Create an user registration handling class.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,9 @@
 'auth.py' contains a function for passwords hashing
 """
 from bcrypt import gensalt, hashpw
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(pwd: str) -> bytes:
@@ -12,3 +15,23 @@ def _hash_password(pwd: str) -> bytes:
     encoded_pwd = pwd.encode('utf-8')
     salt = gensalt()
     return (hashpw(encoded_pwd, salt))
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        'register_user' processes a user request for registration
+        """
+        try:
+            if self._db.find_user_by(email=email):
+                raise ValueError(f'User {email} already exists.')
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return (user)
