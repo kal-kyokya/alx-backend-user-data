2c7e4584aaf8ce6a032724d1545c6754c1d1da33
kal-kyokya <kalkyokya4@gmail.com>
2024-11-19 14:55:10 +0000

    Define a method that takes in a password and returns a salted hash of the input.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,7 @@
 from bcrypt import gensalt, hashpw
 
 
-def _hash_password(pwd: str) -> str:
+def _hash_password(pwd: str) -> bytes:
     """
     '_hash_password' hashes any string input password.
     """
