933c66da624621f0a3704ca7e6c173a72ac1ddaa
kal-kyokya <kalkyokya4@gmail.com>
2024-11-20 09:27:06 +0000

    Create a method that validates a user's credentials.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,7 @@
 """
 'auth.py' contains a function for passwords hashing
 """
-from bcrypt import gensalt, hashpw
+from bcrypt import checkpw, gensalt, hashpw
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
@@ -35,3 +35,13 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return (user)
+
+    def valid_login(self, email: str, pwd: str) -> bool:
+        """
+        'valid_login' matches a user's credential with the database.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            return (checkpw(pwd.encode('utf-8'), user.hashed_password))
+        except NoResultFound:
+            return (False)
