810ef3cfa22742ccead0c72439ba9b7ecc8d7989
kal-kyokya <kalkyokya4@gmail.com>
2024-11-20 10:08:47 +0000

    Create a session generating method.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -53,3 +53,15 @@ class Auth:
             return (checkpw(pwd.encode('utf-8'), user.hashed_password))
         except NoResultFound:
             return (False)
+
+    def create_session(self, email: str) -> str:
+        """
+        'create_session' assigns a session ID to a specific user.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            user.session_id = session_id
+            return (user.session_id)
+        except NoResultFound:
+            return (None)
