41ff2630667d243c806c6fe4d8dea1333c418d80
kal-kyokya <kalkyokya4@gmail.com>
2024-11-22 10:28:32 +0000

    define a _hash_password method that takes in a password string arguments and returns bytes.

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -9,13 +9,13 @@ from user import User
 import uuid
 
 
-def _hash_password(pwd: str) -> bytes:
+def _hash_password(password: str) -> bytes:
     """
     '_hash_password' hashes any string input password.
     """
-    encoded_pwd = pwd.encode('utf-8')
+    encoded_password = password.encode('utf-8')
     salt = gensalt()
-    return (hashpw(encoded_pwd, salt))
+    return (hashpw(encoded_password, salt))
 
 
 def _generate_uuid() -> str:
