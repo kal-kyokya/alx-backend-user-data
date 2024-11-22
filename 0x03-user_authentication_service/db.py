42536e4db96707ceaa9ccc8d93a8d80c0ab2aad7
kal-kyokya <kalkyokya4@gmail.com>
2024-11-22 10:02:22 +0000

    Implement a method that saves a user to its database.

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -36,12 +36,12 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_pwd: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         'add_user' saves the input user to the database.
         """
         try:
-            new_user = User(email=email, hashed_password=hashed_pwd)
+            new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
             self._session.commit()
         except Exception:
